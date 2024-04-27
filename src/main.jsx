bd8d2896dfbfc6fd7fffdf0811318170c6226722
ganapativs <vsg.inbox@gmail.com>
2024-04-27 13:21:18 +0530

    Fix FOUC

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,24 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import FontFaceObserver from "fontfaceobserver";
 import App from "./App.jsx";
 import "@radix-ui/themes/styles.css";
 import "./index.css";
+
+/// Show the body once the fonts are loaded
+const schibstedGroteskObserver = new FontFaceObserver("Schibsted Grotesk");
+const OutfitObserver = new FontFaceObserver("Outfit");
+
+Promise.all([
+  schibstedGroteskObserver.load(null, 250),
+  OutfitObserver.load(null, 250),
+])
+  .catch((e) => {
+    console.error(e);
+  })
+  .finally(() => {
+    document.body.style.display = "block";
+  });
 
 /**
  * TODO
